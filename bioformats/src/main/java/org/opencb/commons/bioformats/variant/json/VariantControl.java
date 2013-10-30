43e528f3a90f786925bcf67e6dd5dc93f9402619
Alejandro Alemán <aaleman@cipf.es>
2013-10-30 16:59:31 +0100

    Moved the code from sqliteIndex/Stats/Effect to Opencga-storage, and put the code from VariantInfo/Analysis/Control to bioformats

---
+++ bioformats/src/main/java/org/opencb/commons/bioformats/variant/json/VariantControl.java
@@ -0,0 +1,45 @@
+package org.opencb.commons.bioformats.variant.json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;import java.lang.String;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: aaleman
+ * Date: 10/1/13
+ * Time: 3:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class VariantControl {
+
+    @JsonProperty
+    private float maf;
+
+    @JsonProperty
+    private String allele;
+
+    VariantControl(float maf, String allele) {
+        this.maf = maf;
+        this.allele = allele;
+    }
+
+    public VariantControl() {
+        this.maf = -1;
+        this.allele = "";
+    }
+
+    public float getMaf() {
+        return maf;
+    }
+
+    public void setMaf(float maf) {
+        this.maf = maf;
+    }
+
+    public String getAllele() {
+        return allele;
+    }
+
+    public void setAllele(String allele) {
+        this.allele = allele;
+    }
+}
