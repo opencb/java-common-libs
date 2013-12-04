9b3ac1542d4c33b2b4aa8f4bd36a3455e16271aa
Alejandro Alemán <aaleman@cipf.es>
2013-12-04 13:05:42 +0100

    Added ObjectMap,QueryOptions & QueryResults to containers

---
+++ commons/src/main/java/org/opencb/commons/containers/QueryOptions.java
@@ -0,0 +1,22 @@
+package org.opencb.commons.containers;
+
+
+public class QueryOptions extends ObjectMap {
+
+
+    private static final long serialVersionUID = -6331081481906004636L;
+
+
+    public QueryOptions() {
+
+    }
+
+    public QueryOptions(String key, Object value) {
+        super(key, value);
+    }
+
+    public QueryOptions(String json) {
+        super(json);
+    }
+
+}
