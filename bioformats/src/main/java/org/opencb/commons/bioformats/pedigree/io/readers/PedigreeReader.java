0fff2dbcc017e64675a47b4bbb211a7d33fc6de2
Alejandro Alemán <aaleman@cipf.es>
2014-02-10 11:13:29 +0100

    Changed PedDataReader/Writer to PedigreeReader/Writer

---
+++ bioformats/src/main/java/org/opencb/commons/bioformats/pedigree/io/readers/PedigreeReader.java
@@ -0,0 +1,11 @@
+package org.opencb.commons.bioformats.pedigree.io.readers;
+
+
+import org.opencb.commons.bioformats.pedigree.Pedigree;
+import org.opencb.commons.io.DataReader;
+
+/**
+ * @author Alejandro Aleman Ramos <aaleman@cipf.es>
+ */
+public interface PedigreeReader extends DataReader<Pedigree> {
+}
