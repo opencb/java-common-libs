e710fe1b03593fc2c85a2fcf19e6fa6e7d3d2d1c
Jacobo Coll Moragón <jacobo167@gmail.com>
2014-02-19 18:38:55 +0100

    Minor changes: Files moved to new location. Some Files removed.

---
+++ bioformats/src/main/java/org/opencb/commons/bioformats/alignment/stats/AlignmentCoverage.java
@@ -0,0 +1,59 @@
+package org.opencb.commons.bioformats.alignment.stats;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jcoll
+ * Date: 12/4/13
+ * Time: 6:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AlignmentCoverage {
+
+    private String chromosome;
+    private int start;
+    private int end;
+    private float coverage;
+
+    public AlignmentCoverage() {
+        this("",-1,-1,-1);
+    }
+
+    public AlignmentCoverage(String chromosome, int start, int end, float coverage) {
+        this.chromosome = chromosome;
+        this.start = start;
+        this.end = end;
+        this.coverage = coverage;
+    }
+
+    public String getChromosome() {
+        return chromosome;
+    }
+
+    public void setChromosome(String chromosome) {
+        this.chromosome = chromosome;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    public float getCoverage() {
+        return coverage;
+    }
+
+    public void setCoverage(float coverage) {
+        this.coverage = coverage;
+    }
+}
