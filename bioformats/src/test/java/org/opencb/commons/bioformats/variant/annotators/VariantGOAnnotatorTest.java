167b71bb3698401d1ce648aac7bbee729dfbfcee
Alejandro Alemán <aaleman@cipf.es>
2014-08-28 14:21:14 +0200

    Removed test from VariantGOAnnotatorTest

--- bioformats/src/test/java/org/opencb/commons/bioformats/variant/annotators/VariantGOAnnotatorTest.java
+++ bioformats/src/test/java/org/opencb/commons/bioformats/variant/annotators/VariantGOAnnotatorTest.java
@@ -19,46 +19,40 @@ public class VariantGOAnnotatorTest {
     public void testAnnot() throws Exception {
 
 
-//        String inputFile = getClass().getResource("/variant-test-file.vcf.gz").getFile();
-//        String output = "/tmp/out.vcf";
-//
-//        VariantReader reader = new VariantVcfReader(inputFile);
-//        VariantWriter writer = new VariantVcfDataWriter(reader, output);
-//
-//        VariantAnnotator goAnnotator = new VariantGOAnnotator();
-//        VariantAnnotator geneAnnotator = new VariantGeneNameAnnotator();
-//        reader.open();
-//        writer.open();
-//        reader.pre();
-//        writer.pre();
-//
-//        List<Variant> batch = reader.read(500);
-//
-//        while (!batch.isEmpty()) {
-//
-//            geneAnnotator.annot(batch);
-//            goAnnotator.annot(batch);
-//
-//            writer.write(batch);
-//
-//            batch.clear();
-//            batch = reader.read(500);
-//        }
-//
-//        reader.post();
-//        writer.post();
-//        reader.close();
-//        writer.close();
-//
-//        assertTrue(1 == 1);
-        this.prueba();
+        String inputFile = getClass().getResource("/variant-test-file.vcf.gz").getFile();
+        String output = "/tmp/out.vcf";
+
+        VariantReader reader = new VariantVcfReader(inputFile);
+        VariantWriter writer = new VariantVcfDataWriter(reader, output);
+
+        VariantAnnotator goAnnotator = new VariantGOAnnotator();
+        VariantAnnotator geneAnnotator = new VariantGeneNameAnnotator();
+        reader.open();
+        writer.open();
+        reader.pre();
+        writer.pre();
+
+        List<Variant> batch = reader.read(500);
+
+        while (!batch.isEmpty()) {
+
+            geneAnnotator.annot(batch);
+            goAnnotator.annot(batch);
+
+            writer.write(batch);
+
+            batch.clear();
+            batch = reader.read(500);
+        }
+
+        reader.post();
+        writer.post();
+        reader.close();
+        writer.close();
+
+        assertTrue(1 == 1);
 
     }
 
-    public void prueba(){
-        TreeSet<String> x = new TreeSet<>();
-        x.add("ZZ");
-        DefaultTreeN
 
-    }
 }
