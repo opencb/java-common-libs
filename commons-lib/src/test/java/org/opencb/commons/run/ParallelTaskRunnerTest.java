8776797dad97d4bb249aba8323c919f26f90968b
Jacobo Coll Moragón <jacobo167@gmail.com>
2015-04-09 17:23:55 +0100

    ParallelTaskRunner accepts a null DataReader.

--- commons-lib/src/test/java/org/opencb/commons/run/ParallelTaskRunnerTest.java
+++ commons-lib/src/test/java/org/opencb/commons/run/ParallelTaskRunnerTest.java
@@ -12,6 +12,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -41,35 +43,36 @@ public class ParallelTaskRunnerTest {
         Files.delete(Paths.get(outputFileName));
     }
 
+    final Long[] l = {0l, 0l, 0l};
+    ParallelTaskRunner.Task<String, Integer> wc = strings -> {
+        List<Integer> list = new ArrayList<>(strings.size());
+        long lines = 0, words = 0, chars = 0;
+        for (String string : strings) {
+            if ((lines & 63) == 0) {
+                System.out.println("->" + (l[0]+lines));
+            }
+            list.add(string.length());
+            lines++;                                     //lines
+            words += string.split("[\n\t ]").length;     //words
+            chars += string.length() + 1;                //chars
+            for (int i = 1; i < 100; i++) {
+                int ignored = (int)(string.length()*0.5+5*Math.log10(string.split("[\n\t ]").length*50.0*Math.abs(Math.sin(l[1]))));  //stupid operation
+            }
+        }
+        synchronized (l) {
+            l[0] += lines;
+            l[1] += words;
+            l[2] += chars;
+        }
+        return list;
+    };
     @Test
     public void test() throws Exception {
         ParallelTaskRunner.Config config = new ParallelTaskRunner.Config(8, 100, 10, false);
 
-        final Long[] l = {0l, 0l, 0l};
+        l[0] = l[1] = l[2] = 0l;
         Path path = Paths.get(fileName);
 
-        ParallelTaskRunner.Task<String, Integer> wc = strings -> {
-            LinkedList<Integer> list = new LinkedList<>();
-            long lines = 0, words = 0, chars = 0;
-            for (String string : strings) {
-                if ((lines & 63) == 0) {
-                    System.out.println("->" + (l[0]+lines));
-                }
-                list.add(string.length());
-                lines++;                                     //lines
-                words += string.split("[\n\t ]").length;     //words
-                chars += string.length() + 1;                //chars
-                for (int i = 10; i < 100; i++) {
-                    int ignored = (int)(string.length()*0.5+5*Math.log10(string.split("[\n\t ]").length*50.0*Math.abs(Math.sin(l[1]))));  //stupid operation
-                }
-            }
-            synchronized (l) {
-                l[0] += lines;
-                l[1] += words;
-                l[2] += chars;
-            }
-            return list;
-        };
 
         DataOutputStream os = new DataOutputStream(new FileOutputStream(outputFileName));
         DataWriter<Integer> dataWriter = batch -> {
@@ -85,6 +88,55 @@ public class ParallelTaskRunnerTest {
         };
 
         ParallelTaskRunner<String, Integer> runner = new ParallelTaskRunner<>(new StringDataReader(path), wc, dataWriter, config);
+        runner.run();
+        System.out.println("WC : " + l[0] + " " + l[1] + " " + l[2]);
+
+        os.close();
+
+    }
+
+
+
+    @Test
+    public void testWithoutReader() throws Exception {
+        ParallelTaskRunner.Config config = new ParallelTaskRunner.Config(8, 100, 10, false);
+
+        l[0] = l[1] = l[2] = 0l;
+        Path path = Paths.get(fileName);
+        final int[] generatedLines = {0};
+
+        ParallelTaskRunner.Task<String, Integer> generateAndwc = strings -> {
+            //Generate data
+            int linesToGenerate;
+            synchronized (config) {
+                linesToGenerate = lines - generatedLines[0] > 100 ? 100 : Math.max(lines - generatedLines[0], 0);
+                generatedLines[0] += linesToGenerate;
+            }
+            strings = new ArrayList<>(linesToGenerate);
+            for (int i = 0; i < linesToGenerate; i++) {
+                strings.add(new StringBuilder()
+                        .append(RandomStringUtils.randomAlphanumeric(RandomUtils.nextInt(16))).append(" ")
+                        .append(RandomStringUtils.randomAlphanumeric(RandomUtils.nextInt(16))).append(" ")
+                        .append(RandomStringUtils.randomAlphanumeric(RandomUtils.nextInt(16))).append("\n")
+                        .toString());
+            }
+            return wc.apply(strings);
+        };
+
+        DataOutputStream os = new DataOutputStream(new FileOutputStream(outputFileName));
+        DataWriter<Integer> dataWriter = batch -> {
+            for (Integer integer : batch) {
+                try {
+                    os.writeBytes(integer + "\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+            }
+            return true;
+        };
+
+        ParallelTaskRunner<String, Integer> runner = new ParallelTaskRunner<>(null, generateAndwc, dataWriter, config);
         runner.run();
         System.out.println("WC : " + l[0] + " " + l[1] + " " + l[2]);
 
