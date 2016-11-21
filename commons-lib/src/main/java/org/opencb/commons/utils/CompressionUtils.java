829ade56d5969eb0e865968d1806cd01e62ab41f
Jacobo Coll Moragón <jacobo167@gmail.com>
2016-11-21 12:41:12 +0000

    commons: Fix CompressionUtils. Close Deflater and Inflater
    It causes a memory leak in JNI.

--- commons-lib/src/main/java/org/opencb/commons/utils/CompressionUtils.java
+++ commons-lib/src/main/java/org/opencb/commons/utils/CompressionUtils.java
@@ -17,40 +17,43 @@ public final class CompressionUtils {
     }
 
     public static byte[] compress(byte[] data) throws IOException {
+
         Deflater deflater = new Deflater();
-        deflater.setInput(data);
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length)) {
+            deflater.setInput(data);
+            deflater.finish();
+            byte[] buffer = new byte[1024];
+            while (!deflater.finished()) {
+                int count = deflater.deflate(buffer); // returns the generated code... index
+                outputStream.write(buffer, 0, count);
+            }
+            byte[] output = outputStream.toByteArray();
 
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
-
-        deflater.finish();
-        byte[] buffer = new byte[1024];
-        while (!deflater.finished()) {
-            int count = deflater.deflate(buffer); // returns the generated code... index
-            outputStream.write(buffer, 0, count);
+            Logger.getLogger(CompressionUtils.class.getName()).log(Level.FINE, "Original: {0} Kb", data.length / 1024);
+            Logger.getLogger(CompressionUtils.class.getName()).log(Level.FINE, "Compressed: {0} Kb", output.length / 1024);
+            return output;
+        } finally {
+            deflater.end();
         }
-        outputStream.close();
-        byte[] output = outputStream.toByteArray();
-
-        Logger.getLogger(CompressionUtils.class.getName()).log(Level.FINE, "Original: {0} Kb", data.length / 1024);
-        Logger.getLogger(CompressionUtils.class.getName()).log(Level.FINE, "Compressed: {0} Kb", output.length / 1024);
-        return output;
     }
 
     public static byte[] decompress(byte[] data) throws IOException, DataFormatException {
         Inflater inflater = new Inflater();
-        inflater.setInput(data);
 
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
-        byte[] buffer = new byte[1024];
-        while (!inflater.finished()) {
-            int count = inflater.inflate(buffer);
-            outputStream.write(buffer, 0, count);
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length)) {
+            inflater.setInput(data);
+            byte[] buffer = new byte[1024];
+            while (!inflater.finished()) {
+                int count = inflater.inflate(buffer);
+                outputStream.write(buffer, 0, count);
+            }
+            byte[] output = outputStream.toByteArray();
+
+            Logger.getLogger(CompressionUtils.class.getName()).log(Level.FINE, "Original: {0}", data.length);
+            Logger.getLogger(CompressionUtils.class.getName()).log(Level.FINE, "Compressed: {0}", output.length);
+            return output;
+        } finally {
+            inflater.end();
         }
-        outputStream.close();
-        byte[] output = outputStream.toByteArray();
-
-        Logger.getLogger(CompressionUtils.class.getName()).log(Level.FINE, "Original: {0}", data.length);
-        Logger.getLogger(CompressionUtils.class.getName()).log(Level.FINE, "Compressed: {0}", output.length);
-        return output;
     }
 }
