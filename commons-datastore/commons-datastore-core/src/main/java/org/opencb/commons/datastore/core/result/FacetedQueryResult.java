f2d525fa46f083ff127ad650b5fac8920e9e085b
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2018-10-01 10:26:44 +0100

    datastore-core: update faceted query result

--- commons-datastore/commons-datastore-core/src/main/java/org/opencb/commons/datastore/core/result/FacetedQueryResult.java
+++ commons-datastore/commons-datastore-core/src/main/java/org/opencb/commons/datastore/core/result/FacetedQueryResult.java
@@ -24,24 +24,24 @@ import java.util.List;
  */
 public class FacetedQueryResult extends AbstractResult {
 
-    private int numResults;
+    private String query;
     private FacetedQueryResultItem result;
 
     public FacetedQueryResult() {
     }
 
     @Deprecated
-    public FacetedQueryResult(String id, int dbTime, int numResults, long numTotalResults, String warningMsg, String errorMsg,
-                              FacetedQueryResultItem result) {
-        this(id, dbTime, numResults, numTotalResults, Collections.singletonList(new Error(-1, "", warningMsg)), new Error(-1, "", errorMsg),
-                result);
+    public FacetedQueryResult(String id, int dbTime, long numTotalResults, String warningMsg, String errorMsg,
+                              FacetedQueryResultItem result, String query) {
+        this(id, dbTime, numTotalResults, Collections.singletonList(new Error(-1, "", warningMsg)), new Error(-1, "", errorMsg),
+                result, query);
     }
 
-    public FacetedQueryResult(String id, int dbTime, int numResults, long numMatches, List<Error> warning, Error error,
-                              FacetedQueryResultItem result) {
+    public FacetedQueryResult(String id, int dbTime, long numMatches, List<Error> warning, Error error,
+                              FacetedQueryResultItem result, String query) {
         super(id, dbTime, numMatches, warning, error);
-        this.numResults = numResults;
         this.result = result;
+        this.query = query;
     }
 
     @Override
@@ -49,43 +49,21 @@ public class FacetedQueryResult extends AbstractResult {
         final StringBuilder sb = new StringBuilder("FacetedQueryResult{");
         sb.append("id='").append(id).append('\'');
         sb.append(", dbTime=").append(dbTime);
-        sb.append(", numResults=").append(numResults);
         sb.append(", numMatches=").append(numMatches);
         sb.append(", warning='").append(warning).append('\'');
         sb.append(", error='").append(error).append('\'');
+        sb.append(", query=").append(query);
         sb.append(", result=").append(result);
         sb.append('}');
         return sb.toString();
     }
 
-    public int getNumResults() {
-        return numResults;
+    public String getQuery() {
+        return query;
     }
 
-    public FacetedQueryResult setNumResults(int numResults) {
-        this.numResults = numResults;
-        return this;
-    }
-
-    @Deprecated
-    public long getNumTotalResults() {
-        return numMatches;
-    }
-
-    @Deprecated
-    public FacetedQueryResult setNumTotalResults(long numTotalResults) {
-        this.numMatches = numTotalResults;
-        return this;
-    }
-
-    @Override
-    public long getNumMatches() {
-        return numMatches;
-    }
-
-    @Override
-    public FacetedQueryResult setNumMatches(long numMatches) {
-        this.numMatches = numMatches;
+    public FacetedQueryResult setQuery(String query) {
+        this.query = query;
         return this;
     }
 
