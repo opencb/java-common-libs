caf6cace7c12299932b69899f9eec39e1a6b9cd1
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2020-02-17 12:31:59 +0000

    commons: add 'user' parameter to the DockerUtils.run(), #45

--- commons-lib/src/main/java/org/opencb/commons/utils/DockerUtils.java
+++ commons-lib/src/main/java/org/opencb/commons/utils/DockerUtils.java
@@ -1,5 +1,6 @@
 package org.opencb.commons.utils;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.opencb.commons.exec.Command;
 
@@ -7,9 +8,27 @@ import java.util.List;
 
 public class DockerUtils {
 
-    public static void run(String image, List<Pair<String, String>> bindings, String params) {
+    /**
+     * Create and run the command line to execute the docker image.
+     *
+     * @param image Docker image name
+     * @param user  Array of two strings: the first string, the user; the second, the group
+     * @param bindings Array of bind mounts for docker volumes: source-target pairs
+     * @param params Image command parameter
+     * @return The command line
+     */
+    public static String run(String image, String[] user, List<Pair<String, String>> bindings, String params) {
         // Docker run
         StringBuilder commandLine = new StringBuilder("docker run ");
+
+        // User
+        if (ArrayUtils.isNotEmpty(user)) {
+            String u = user[0];
+            if (user.length > 1) {
+                u += (":" + user[1]);
+            }
+            commandLine.append("-u ").append(u).append(" ");
+        }
 
         // Mount management (bindings)
         for (Pair<String, String> binding : bindings) {
@@ -21,11 +40,13 @@ public class DockerUtils {
         // Docker image and version
         commandLine.append(image).append(" ");
 
-        // Params
+        // Image command params
         commandLine.append(params);
 
         // Execute command
         Command cmd = new Command(commandLine.toString());
         cmd.run();
+
+        return commandLine.toString();
     }
 }
