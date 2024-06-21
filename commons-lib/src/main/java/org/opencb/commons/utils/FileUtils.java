73e02fedf35e55d41fb762456b3f2ed75d62aef8
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2024-06-21 18:52:31 +0200

    utils: improve log messages, #TASK-6165

--- commons-lib/src/main/java/org/opencb/commons/utils/FileUtils.java
+++ commons-lib/src/main/java/org/opencb/commons/utils/FileUtils.java
@@ -211,9 +211,9 @@ public final class FileUtils {
     }
 
     public static boolean existsFile(File file) {
-        logger.info("file = {}; Files.exists ? {}", file, Files.exists(file.toPath()));
+        logger.info("FileUtils:existsFile: file = {}; Files.exists ? {}", file, Files.exists(file.toPath()));
         URI uri = file.toURI();
-        logger.info("uri = {}; Files.exists ? {}", uri, Files.exists(Paths.get(uri)));
+        logger.info("FileUtils:existsFile: uri = {}; Files.exists ? {}", uri, Files.exists(Paths.get(uri)));
 
         try (InputStream is = Files.newInputStream(file.toPath())) {
             return true;
